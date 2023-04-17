f46ef58b6822bf618dc0ce84796b62adec87dcc2
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-17 16:29:58 +0545

    pagination and styling

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,17 +1,16 @@
 import { useState } from "react";
+import ReactPaginate from "react-paginate";
 import useFetch from "./useFetch";
 
 const Items = (props) => {
+  const [pageNumber, setPageNumber] = useState(0);
   const { playlistId } = props;
-  const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=${playlistId}&key=${process.env.REACT_APP_API_KEY}`;
+  const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=20&playlistId=${playlistId}&key=${process.env.REACT_APP_API_KEY}`;
   const [detail, setDetail] = useState();
   const handleClick = (id) => {
-    console.log(id);
     setDetail(id);
   };
-
   const { data, isPending, error } = useFetch(url);
-  console.log(data, "items");
 
   if (isPending) {
     return <div className="loading"></div>;
@@ -26,24 +25,47 @@ const Items = (props) => {
       </div>
     );
   }
+  const itemsPerPage = 4; // number of items to display per page
+  const pagesVisited = pageNumber * itemsPerPage;
+  const pageCount = Math.ceil(data.length / itemsPerPage);
+
+  const displayData = data.slice(pagesVisited, pagesVisited + itemsPerPage); // slice the data to display only items for the current page
+
+  const handlePageChange = ({ selected }) => {
+    setPageNumber(selected); // update the current page number when the user clicks on a new page
+  };
 
   return (
     <div>
       <div className="playlist-grid">
-        {data.map((item, index) => (
+        {displayData.map((item) => (
           <div
             key={item.id}
             className="playlist-cards"
+            style={{ height: detail === item?.etag ? "500px" : "300px" }}
             onClick={() => handleClick(item.etag)}
           >
-            <img
+            {/* <img
               src={item.snippet.thumbnails.high.url}
               alt={item.snippet.title}
               className="thumbnail"
-            />
+            /> */}
             <div className="info">
-              {detail === item?.etag ? (
+              {detail !== item?.etag ? (
                 <>
+                  <img
+                    src={item.snippet.thumbnails.high.url}
+                    alt={item.snippet.title}
+                    className="thumbnail"
+                  />
+                </>
+              ) : (
+                <>
+                  <img
+                    src={item.snippet.thumbnails.high.url}
+                    alt={item.snippet.title}
+                    className="thumbnails"
+                  />
                   <h2 className="title">{item.snippet.title}</h2>
                   <p className="author">Author:{item.snippet.channelTitle}</p>
                   <p className="publishedDate">
@@ -55,13 +77,19 @@ const Items = (props) => {
                     {item.snippet.description}
                   </p>
                 </>
-              ) : (
-                <></>
               )}
             </div>
           </div>
         ))}
       </div>
+      <ReactPaginate
+        pageCount={pageCount} // total number of pages
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={10}
+        onPageChange={handlePageChange}
+        containerClassName="pagination"
+        activeClassName="active"
+      />
     </div>
   );
 };
--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
 import useFetch from "./useFetch";
 import Items from "./Items";
+import ReactPaginate from "react-paginate";
+import "./style.css";
 
 const Mapping = () => {
-  const [playlistId, setPlaylistId] = useState(null); // initial state is null
+  const [playlistId, setPlaylistId] = useState(null);
+  const [pageNumber, setPageNumber] = useState(0); // add a new state for page number
 
   const handleClick = (id) => {
     setPlaylistId(id);
   };
 
   const { data, isPending, error } = useFetch(
-    `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&channelId=${process.env.REACT_APP_CHANNELID}&maxResults=5&key=${process.env.REACT_APP_API_KEY}`
+    `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&channelId=${process.env.REACT_APP_CHANNELID}&maxResults=20&key=${process.env.REACT_APP_API_KEY}`
   );
+
+  const itemsPerPage = 4; // number of items to display per page
+  const pagesVisited = pageNumber * itemsPerPage;
+  const pageCount = Math.ceil(data.length / itemsPerPage);
+
+  const displayData = data.slice(pagesVisited, pagesVisited + itemsPerPage); // slice the data to display only items for the current page
+
+  const handlePageChange = ({ selected }) => {
+    setPageNumber(selected); // update the current page number when the user clicks on a new page
+  };
 
   if (isPending) {
     return <div className="loading"></div>;
@@ -36,24 +49,36 @@ const Mapping = () => {
         {playlistId ? (
           playlistId && <Items playlistId={playlistId} />
         ) : (
-          <div className="playlist-grid">
-            {data.map((item) => (
-              <div
-                key={item.id}
-                className="playlist-card"
-                onClick={() => handleClick(item.id)}
-              >
-                <img
-                  src={item.snippet.thumbnails.high.url}
-                  alt={item.snippet.title}
-                  className="thumbnail"
-                />
-                <div className="info">
-                  <h2 className="title">{item.snippet.title}</h2>
-                  <p className="description">{item.snippet.description}</p>
+          <div className="main">
+            <div className="playlist-grid">
+              {displayData.map((item) => (
+                <div
+                  key={item.id}
+                  className="playlist-card"
+                  onClick={() => handleClick(item.id)}
+                >
+                  <img
+                    src={item.snippet.thumbnails.high.url}
+                    alt={item.snippet.title}
+                    className="thumbnail"
+                  />
+                  <div className="info">
+                    <h2 className="title">{item.snippet.title}</h2>
+                    {/* <p className="description">{item.snippet.description}</p> */}
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+            <div className="pagee">
+              <ReactPaginate
+                pageCount={pageCount} // total number of pages
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={10}
+                onPageChange={handlePageChange}
+                containerClassName="pagination"
+                activeClassName="active"
+              />
+            </div>
           </div>
         )}
       </div>
